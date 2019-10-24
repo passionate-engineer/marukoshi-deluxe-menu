2a61b44bbb2e98b0fbe2d8f8aa570bfef520a4d8
Passionate Engineer <ryoju.builder@gmail.com>
2019-10-24 12:47:05 +0900

    Fix code comment

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
-const fs = require('fs');
+const fs = require("fs");
 const Jimp = require("jimp");
 const PDF2Pic = require("pdf2pic");
 const Tesseract = require("node-tesseract-ocr");
 
-// https://ai-facets.org/tesseract-ocr-best-practices/
 const tesseractConfig = {
   lang: "jpn",
-  oem: 1,
+  oem: 1, // https://ai-facets.org/tesseract-ocr-best-practices/
   psm: 1
 };
 
+/**
+ * Convert the pdf to texts and output json file.
+ */
 const main = async () => {
   const calendarTexts = await getCalendarTexts(
     "./resources/deluxe1.pdf",
     "./resources/"
   );
   console.log(calendarTexts);
-  fs.writeFileSync('./resources/output.json', JSON.stringify(calendarTexts, null , "\t"));
+  fs.writeFileSync(
+    "./resources/output.json",
+    JSON.stringify(calendarTexts, null, "\t")
+  );
 };
 
+/**
+ * Get texts by date using image segmentation and OCR from The Deluxe Menu of Marukoshi.
+ * @param {string} pdfPath
+ * @param {string} resourcesDir Working directory.
+ * @return {object} calendarTexts Converted texts by date.
+ */
 const getCalendarTexts = async (pdfPath, resourcesDir) => {
   let calendarTexts = {};
 
@@ -45,9 +56,12 @@ const getCalendarTexts = async (pdfPath, resourcesDir) => {
       const result = await Tesseract.recognize(imagePath, tesseractConfig);
       const date = result.match(/^\d+月\d+日/);
       if (date) {
-        calendarTexts[date[0]] = result.replace(date[0] + '\n', '').replace(/\n\n/g, '\n').trim();
+        calendarTexts[date[0]] = result
+          .replace(date[0] + "\n", "")
+          .replace(/\n\n/g, "\n")
+          .trim();
         console.log(date[0]);
-        console.log(calendarTexts[date[0]] + '\n');
+        console.log(calendarTexts[date[0]] + "\n");
       }
     }
   }
