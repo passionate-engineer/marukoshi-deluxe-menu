8b18fc1c85fbc53d594a1d91f99281751e599c5a
Passionate Engineer <ryoju.builder@gmail.com>
2019-11-07 16:03:35 +0900

    Fix recognition engine option

--- app.js
+++ app.js
@@ -49,7 +49,9 @@ const getCalendarTexts = async (pdfPath, resourcesDir) => {
 
       const imagePath = resourcesDir + "/clips/" + (i * 5 + j) + ".png";
       await jimp.write(imagePath);
-      const result = await Tesseract.recognize(imagePath, { lang: "jpn" });
+
+      let result = await Tesseract.recognize(imagePath, { lang: "jpn", psm: 6 });
+      result = result.replace('晶');
       const date = result.match(/^\d+月\d+日/);
       console.log(j)
       console.log(result)
